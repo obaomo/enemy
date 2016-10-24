e2042e14c175bf505dd194d642b36c73792ae670
stephen obaido <stevenobaido@yahoo.com>
2016-10-24 17:10:47 -0400

    Update ape.js

--- js/ape.js
+++ js/ape.js
@@ -1,20 +1,13 @@
 // Enemies our player must avoid
 var Enemy = function() {
-    var obj = object.create (Enemy.prototype); // Variables applied to each of our instances go here,
-        obj.loc = loc; // we've provided one for you to get started
+    var obj = object.create (Enemy.prototype); 
+        obj.loc = loc; 
         return obj;
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
 };
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
     console.logs  
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
     this.x = (this.x + this.speed);
     this.y = 83*this.row;
 
@@ -35,13 +28,10 @@ Enemy.prototype.return = function() {
     this.y = 83 *this.row;
     tis.speed = getRandomInt(2,6);
 }
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
 
 var Player = function() {
-    var obj = object.create (Player.prototype); // Variables applied to each of our instances go here,
-        obj.loc = loc; // we've provided one for you to get started
+    var obj = object.create (Player.prototype); 
+        obj.loc = loc; 
         return obj;
 
 this.sprite = "images/char-boy.png";
@@ -65,14 +55,8 @@ var Player = function() {
 var Enemy = new enemy({x: 30, y: 60})
 console.log(Enemy.loc
 
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
 var allEnemies = [new Enemy(), new Enemy(), new Enemy()];
 
-
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
     var allowedKeys = {
         37: 'left',
